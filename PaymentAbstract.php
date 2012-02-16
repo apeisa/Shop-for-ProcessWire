305aecdc40b7464455cc262c3144858ee7ef41d3
Antti Peisa <antti.peisa@gmail.com>
2012-02-16 02:07:49 +0200

    again big update, customer fields, confirmation page, simple validation

--- PaymentAbstract.php
+++ PaymentAbstract.php
@@ -13,6 +13,22 @@ abstract class PaymentAbstract extends WireData implements Module {
 		$this->completedUrl = '../' . $data['completedUrlSegment'] . '/';
 	}
 	
+	/*
+	 *
+	 * returns nothing. You should edit and save $order page. If payment was succesful,
+	 * then do $order->removeStatus(Page::statusUnpublished) and remember to save the order!
+	 *
+	 * If order was also paid, then do $order->sc_paid = time();
+	 *
+	 * If order was not paid, but it was succesful (like invoice, money on delivery etc)
+	 * then just publish the order, but do not set sc_paid value.
+	 *
+	 * After you have manipulated the order, then just to redirect to $this->completedUrl
+	 *
+	 *
+	 * @param Page $order keeps the page object for the order
+	 *
+	 */
 	abstract function processPayment(Page $order);
 	
 }
